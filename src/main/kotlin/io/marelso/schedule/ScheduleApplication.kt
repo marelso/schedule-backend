ec52188890d702f8ced8414fabffd0bae2007f36
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-05-27 18:32:39 -0300

    Including base app

---
+++ src/main/kotlin/io/marelso/schedule/ScheduleApplication.kt
@@ -0,0 +1,11 @@
+package io.marelso.schedule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScheduleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScheduleApplication>(*args)
+}
