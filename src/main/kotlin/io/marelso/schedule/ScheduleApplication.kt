da0eafddb786d49d945f2648eb52d52ca6035dce
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2025-06-03 21:08:15 -0300

    Revert "Removing page abstraction warning"
    This reverts commit 1a1225b8bfa73cd35c3b74e44397e911eaea9484.

--- src/main/kotlin/io/marelso/schedule/ScheduleApplication.kt
+++ src/main/kotlin/io/marelso/schedule/ScheduleApplication.kt
@@ -2,10 +2,8 @@ package io.marelso.schedule
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.web.config.EnableSpringDataWebSupport
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 class ScheduleApplication
 
 fun main(args: Array<String>) {
